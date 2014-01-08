e68697fa6ccf87483bc0298459cce90c4c85cedd
Tvli <526864480@qq.com>
2014-01-08 23:51:17 +0000

    add player and set background

--- main.py
+++ main.py
@@ -1,17 +1,42 @@
 import pygame
+from process import process
+from classes import *
 
 pygame.init()
 
-SCREENWIDTH, SCREENHEIGHT = 1024, 768
+SCREENWIDTH, SCREENHEIGHT = 800, 600
 screen = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT), 0, 32)
+
 
 clock = pygame.time.Clock()
 FPS = 24
 
+
+player = Player(SCREENWIDTH / 2.0 - 25, SCREENHEIGHT - 47, "images/player1.bmp")
+
 total_frames = 0
 
+color = 0
+delta = 5
+
 while True:
+	process(player)
+
+	player.motion(SCREENWIDTH, SCREENHEIGHT)
+
 	total_frames += 1
 
+	color += delta
+	if color == 255:
+		delta = -5
+	elif color == 0:
+		delta = 5
+	else:
+		pass
+
+	screen.fill((color,color,color))
+	BaseClass.all_sprites.draw(screen)
+
+
 	pygame.display.flip()
-	clock.tick.(FPS)
+	clock.tick(FPS)
