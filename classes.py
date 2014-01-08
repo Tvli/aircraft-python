2bee399e56ad0c04e7d60ca4452566f98a16a64a
Tvli <526864480@qq.com>
2014-01-08 23:50:37 +0000

    add the player class

--- classes.py
+++ classes.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 class BaseClass(pygame.sprite.Sprite):
 	all_sprites = pygame.sprite.Group()
 
@@ -14,3 +15,45 @@ class BaseClass(pygame.sprite.Sprite):
 
 		self.rect.width
 		self.rect.height 
+
+
+class Player(BaseClass):
+	List = pygame.sprite.Group()
+
+	def __init__(self, x, y, image_string):
+		BaseClass.__init__(self, x, y, image_string)
+		Player.List.add(self)
+		self.velx, self.vely = 0, 0
+
+
+	def motion(self, SCREENWIDTH, SCREENHEIGHT):
+		predicted_x_location = self.rect.x + self.velx
+		predicted_y_location = self.rect.y + self.vely
+
+		if predicted_x_location < 0:
+			self.velx = 0
+		elif predicted_x_location + self.rect.width > SCREENWIDTH:
+			self.velx = 0
+		elif predicted_y_location < 0:
+			self.vely = 0
+		elif predicted_y_location + self.rect.height > SCREENHEIGHT:
+			self.vely = 0
+		else:
+			self.rect.x += self.velx
+			self.rect.y += self.vely
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
